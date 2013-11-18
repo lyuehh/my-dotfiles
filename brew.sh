53edc0990ca872d112f4e8c5d7704493b31f8d92
lyuehh <lyuehh@gmail.com>
2013-11-18 23:34:45 +0800

    remove useless file

--- brew.sh
+++ brew.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 brew update
-brew install git-extras io newlisp node imagemagick tree wget ydict ack
-brew install mysql55
+brew install git-extras io node imagemagick tree wget ack
 brew install enca
 brew install macvim jpeginfo ngrep
 brew install the_silver_searcher
@@ -10,13 +9,12 @@ brew install terminal-notifier
 brew install gist
 
 brew tap phinze/homebrew-cask
+brew install brew-cask
 brew tap homebrew/versions
+brew install mysql55
 
 brew cask install iterm2
-brew install brew-cask
-
 brew cask install alfred app-cleaner mesa-sqlite sublime-text textexpander textmate2 the-unarchiver xscope
-brew cask install  google-chrome bit-torrent-sync bettertouchtool 
-brew cask install dropbox
+brew cask install google-chrome bit-torrent-sync bettertouchtool
 
 # ls -l /usr/local/Library/Formula | grep phinze-cask | awk '{print $9}' | for evil_symlink in $(cat -); do rm -v /usr/local/Library/Formula/$evil_symlink; done
