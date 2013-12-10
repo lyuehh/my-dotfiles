b5554e3b62144d350df1d6e2be953c744830af7d
lyuehh <lyuehh@gmail.com>
2013-12-10 21:29:20 +0800

    add m for macvim

--- alias.zsh
+++ alias.zsh
@@ -1,6 +1,7 @@
 export PATH=$PATH:'/Applications/Racket v5.3.6/bin'
 export PATH=$PATH:'/Users/weiwei/bin/mongodb-osx-x86_64-2.4.8/bin'
 
+alias m="mvim"
 alias locate="mdfind -name"
 alias tree='tree -N'
 alias vaz='vi ~/.zsh.after/alias.zsh'
