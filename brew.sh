744b47a0b9da89f1442048afdb5b8f4db36ad219
lyuehh <lyuehh@gmail.com>
2013-12-16 16:22:50 +0800

    update bew

--- brew.sh
+++ brew.sh
@@ -18,6 +18,7 @@ brew cask install iterm2
 brew cask install alfred app-cleaner mesa-sqlite sublime-text textexpander textmate2 the-unarchiver xscope
 brew cask install google-chrome bit-torrent-sync slate
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql suspicious-package
+brew cask install colorpicker-developer colorpicker-hex
 qlmanage -r
 
 # ls -l /usr/local/Library/Formula | grep phinze-cask | awk '{print $9}' | for evil_symlink in $(cat -); do rm -v /usr/local/Library/Formula/$evil_symlink; done
