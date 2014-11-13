2a490934fbabe022e0e3b38f380be4f15ae351fc
lyuehh <lyuehh@gmail.com>
2014-11-13 09:57:05 +0800

    update bew cask

--- brew.sh
+++ brew.sh
@@ -9,6 +9,7 @@ brew install terminal-notifier
 brew install gist
 brew install astyle
 brew install jq
+brew install gnupg gpg-agent pass
 
 brew tap phinze/homebrew-cask
 brew install brew-cask
@@ -16,9 +17,10 @@ brew install brew-cask
 brew tap homebrew/versions
 #brew install mysql55
 
-brew cask fetch iterm2
-brew cask fetch alfred app-cleaner  sublime-text textexpander textmate2 the-unarchiver xscope
-brew cask fetch google-chrome bit-torrent-sync slate
+brew cask fetch iterm2 skim alfred
+brew cask fetch sublime-text textexpander textmate the-unarchiver xscope
+brew cask fetch slate appcleaner
+brew cask fetch caffeine selfControl
 #brew cask fetch vagrant
 
 brew cask fetch qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql suspicious-package
