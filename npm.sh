8e357ac785b859f27c7a3bfbfe17355306465df1
lyuehh <lyuehh@gmail.com>
2015-04-20 09:38:41 +0800

    update

--- npm.sh
+++ npm.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
 npm install -g gtrs
-npm install -g chengyu
-npm install -g tangshi
-npm install -g songci
-npm install -g pinyinjs
-npm install -g opencc-cli
+#npm install -g chengyu
+#npm install -g tangshi
+#npm install -g songci
+#npm install -g pinyinjs
+#npm install -g opencc-cli
 
-npm install -g coffee-script
+#npm install -g coffee-script
 npm install -g jshint
 npm install -g csslint
 npm install -g clean-css
 npm install -g yo
-npm install -g nodemon
-npm install -g pm2
-npm install -g marked
+#npm install -g nodemon
+#npm install -g pm2
+#npm install -g marked
 npm install -g grunt-cli
 npm install -g js-beautify
 npm install -g jscs
 
 # npm install -g component
 npm install -g bower
+npm install -g localtunnel
 
-npm install -g node-inspector
-npm install -g xml2json-command
+#npm install -g node-inspector
+#npm install -g xml2json-command
 npm install -g hquery
 npm install -g up7niu
 
@@ -31,5 +32,8 @@ npm install -g up7niu
 npm install -g gulp
 npm install -g generator-gulp-webapp
 
+npm install -g puer
+npm install -g watchify
+
 #npm install -g jsbin
-npm install -g underscore-cli
+#npm install -g underscore-cli
